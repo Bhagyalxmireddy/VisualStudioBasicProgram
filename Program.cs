2209743377f5fe994afe4547ba59e14f61d7aea1
Bhagi <puppyreddy1997@gmail.com>
2020-11-20 11:34:46 +0530

    ADDED:Added a Method

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ namespace MyFirstVisualStudioProject
 {
     class Program
     {
+        public void method()
+        {
+            Console.WriteLine("calling the method");
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
