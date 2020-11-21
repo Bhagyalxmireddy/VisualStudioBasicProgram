cbf44a297ba6195dece3e11949ad076c64c45674
Bhagi <puppyreddy1997@gmail.com>
2020-11-21 09:51:18 +0530

    ADDED:Added the Console to call the method

---
+++ SelectionStatements.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstVisualStudioProject
+{
+    class SelectionStatements
+    {
+        static void Main()
+        {
+            int a ;
+            Console.WriteLine("enter a number :");
+            a = Convert.ToInt32(Console.ReadLine());
+            if(a < 0)
+            {
+                Console.WriteLine("entered value is negative");
+            }
+            else
+            {
+                Console.WriteLine("entered value is positive");
+            }
+        }
+    }
+}
---
+++ Switch.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstVisualStudioProject
+{
+    class Switch
+    {
+         static void Main()
+        {
+            double x, y, z,a;
+            Console.WriteLine("enter the X value: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter the y value: ");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.Write("enter a number:");
+            a = (char)Console.Read();
+            switch (a)
+            {
+                case 1:
+                    z = x + y;
+                    Console.WriteLine("{0} + {1} = {2}",x,y,z);
+                    break;
+                case 2:
+                    z = x - y;
+                    Console.WriteLine("{0} - {1} = {2}", x, y, z);
+                    break;
+                case 3:
+                    z = x * y;
+                    Console.WriteLine("{0} *  {1} = {2}", x, y, z);
+                    break;
+                case 4:
+                    z = x / y;
+                    Console.WriteLine("{0} / {1} = {2}", x, y, z);
+                    break;
+                default:
+                    Console.WriteLine("invalid number");
+                    break;
+            }
+
+        }
+
+    }
+}
